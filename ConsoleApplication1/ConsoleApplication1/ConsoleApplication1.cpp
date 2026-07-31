a9d4032d8dedba241cd67a08f754f7e0ee9ce60e
Naoto912 <chuankouzhiren325@gmail.com>
2026-07-31 19:34:58 +0900

    変更

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,21 +1,64 @@
-﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "あなたは誰？!\n";
+    srand(time(NULL));
+
+    // 戦力
+    int allyPower = 100;
+    int enemyPower = 100;
+
+    // 攻撃力
+    int allyAttack = 0;
+    int enemyAttack = 0;
+
+    // 行動
+    // 0 = 攻撃
+    // 1 = 防御
+    int allyAction = rand() % 2;
+    int enemyAction = rand() % 2;
+
+    // 味方を選ぶ
+    int random = rand() % 3;
+
+    if (random == 0)
+    {
+        cout << "味方：剣士" << endl;
+        allyAttack = 30;
+    }
+    else if (random == 1)
+    {
+        cout << "味方：魔法使い" << endl;
+        allyAttack = 25;
+    }
+    else
+    {
+        cout << "味方：召喚士" << endl;
+        allyAttack = 20;
+    }
+
+    // 敵を選ぶ
+    random = rand() % 3;
+
+    if (random == 0)
+    {
+        cout << "敵：剣士" << endl;
+        enemyAttack = 30;
+    }
+    else if (random == 1)
+    {
+        cout << "敵：魔法使い" << endl;
+        enemyAttack = 25;
+    }
+    else
+    {
+        cout << "敵：召喚士" << endl;
+        enemyAttack = 20;
+    }
+
+    return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
